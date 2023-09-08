d3b0b5d12365e65ecc6394bdef8e2892bfe07281
Demivdm <demi.van.der.maarl@hva.nl>
2023-09-08 13:57:13 +0200

    base

---
+++ visite-kaartje/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>{data.page.data.example_title}</h1>
+<slot/>
+<style>
+h2{
+    background-color: #fcba03;
+}
+
+</style>
---
+++ visite-kaartje/src/routes/home/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>{data.page.data.example_title}</h1>
