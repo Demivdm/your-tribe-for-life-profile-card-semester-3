86487d192f48e36ecc3ebc6945fa8ce7b6efc684
Demivdm <demi.van.der.maarl@hva.nl>
2023-09-15 14:06:39 +0200

    fix

--- visite-kaartje/src/routes/+page.svelte
+++ visite-kaartje/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
   /** @type {import('./$types').PageData} */
   export let data;
+  console.log(data);
 </script>
 
 <h1>{data.title}</h1>
-<img src={data.img.url} />
+<img src={data.img.url} alt="profile head" />
 <h2>{data.over}</h2>
-<p>{data.introduction}</p>
-
+<!-- omdat deze data in een array staat heeft het blokhaken nodig en moet het beginnen bij 0, daarna kijk je wat je specifiek uit het array moet ophalen in dit geval de text -->
+<p>{data.introduction[0].text}</p>
+<!-- 
 <pre>
 {JSON.stringify(data, null, 2)}
-</pre>
+</pre> -->
